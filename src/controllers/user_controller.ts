8f3a79a5e189b72f0d081eb42ee53c7ef05ae24e
Daniel <daniellfmanoel@gmail.com>
2025-05-22 11:02:51 -0300

    ✨feat: add Zod validation schemas for user data and update TypeScript configuration

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -1,30 +1,63 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { UsersService } from "../services/user_service";
+import { z, ZodSafeParseError, ZodSafeParseResult, } from "zod/v4"
 
 const usersService = new UsersService();
 
-interface ParamsWithId {
-    id: string;
-}
 
-interface CreateUserBody {
-    name: string;
-    email: string;
-    password: string;
-}
+//Schemas com zod para tipagem e validação dos dados
+const paramsWithIdSchema = z.object({
+    id: z.string().transform((value) => {
+        const parsedId = parseInt(value);
+        if (isNaN(parsedId)) {
+            throw new Error("ID inválido");
+        }
+        return parsedId;
+    })
+})
 
-interface UpdateUserBody {
-    name?: string;
-    email?: string;
-    password?: string;
+const createUserBodySchema = z.object({
+    name: z.string().min(1, "O nome é obrigatório"),
+    email: z.email(),
+    password: z.string().min(6, "A senha deve conter no mínimo 6 caracteres")
+})
+
+const updateUserSchema = z.object({
+    name: z.string().min(1, "O nome é obrigatório"),
+    email: z.email(),
+}).refine(data => Object.keys(data).length > 0, {
+    message: "Pelo menos um campo deve ser fornecido para atualização"
+})
+
+const updateUserPasswordSchema = z.object({
+    currentPassword: z.string().min(6, 'A senha atual deve conter no mínimo 6 caracteres'),
+    newPassword: z.string().min(6, "A nova senha deve conter no mínimo 6 caracteres")
+})
+
+// Extraindo os tipos dos schemas para substituir as interfaces
+type ParamsWithId = z.infer<typeof paramsWithIdSchema>
+type CreateUserBody = z.infer<typeof createUserBodySchema>
+type UpdateUserBody = z.infer<typeof updateUserSchema>
+type UpdatePasswordBody = z.infer<typeof updateUserPasswordSchema>
+
+function formatError(result: ZodSafeParseError<any>) {
+    return z.prettifyError(result.error)
 }
 
 export const usersController = {
     async create(request: FastifyRequest<{ Body: CreateUserBody }>, reply: FastifyReply) {
         try {
-            const { name, email, password } = request.body;
-            const user = await usersService.createUser({ name, email, password });
+            const result = createUserBodySchema.safeParse(request.body);
+
+            if (!result.success) {
+                return reply.status(400).send({
+                    message: "Dados inválidos",
+                    errors: formatError(result)
+                });
+            }
+            const user = await usersService.createUser(result.data);
             return reply.status(201).send(user);
+
         } catch (error) {
             if (error instanceof Error) {
                 return reply.status(400).send({ message: error.message });
@@ -47,14 +80,18 @@ export const usersController = {
 
     async findById(request: FastifyRequest<{ Params: ParamsWithId }>, reply: FastifyReply) {
         try {
-            const { id } = request.params;
-            const userId = parseInt(id);
-
-            if (isNaN(userId)) {
-                return reply.status(400).send({ message: "Invalid ID" });
+            const result = paramsWithIdSchema.safeParse(request.params)
+            if (!result.success) {
+                return reply.status(400).send({
+                    message: "ID inválido",
+                    errors: formatError(result)
+                });
             }
+            const userId = result.data.id
             const user = await usersService.findById(userId);
+
             return reply.status(200).send(user);
+
         } catch (error) {
             if (error instanceof Error) {
                 return reply.status(400).send({ message: error.message });
@@ -64,26 +101,65 @@ export const usersController = {
     },
     async update(request: FastifyRequest<{ Params: ParamsWithId; Body: UpdateUserBody }>, reply: FastifyReply) {
         try {
-            const { id } = request.params;
-            const userId = parseInt(id);
-            const data = request.body;
+            const paramsResult = paramsWithIdSchema.safeParse(request.params)
 
-            if (isNaN(userId)) {
-                return reply.status(400).send({ message: "Invalid ID" });
+            if (!paramsResult.success) {
+                return reply.status(400).send({
+                    message: "ID inválido",
+                    errors: formatError(paramsResult)
+                });
             }
 
-            const user = await usersService.update(userId, {
-                name: data.name,
-                email: data.email,
-                password: data.password,
-            });
+            const bodyResult = updateUserSchema.safeParse(request.body)
 
-            if (data.password && data.password.trim() !== '') {
-                user['password'] = data.password
+            if (!bodyResult.success) {
+                return reply.status(400).send({
+                    message: "Dados inválidos",
+                    errors: formatError(bodyResult)
+                })
             }
 
+            const userId = paramsResult.data.id;
+            const data = bodyResult.data
+
+            const user = await usersService.update(userId, data);
             return reply.send(user);
-            
+
+        } catch (error) {
+            if (error instanceof Error) {
+                return reply.status(400).send({ message: error.message });
+            }
+            return reply.status(500).send({ message: "Erro interno do servidor" });
+        }
+    },
+
+    async updatePassword(request: FastifyRequest<{ Params: ParamsWithId; Body: UpdatePasswordBody }>, reply: FastifyReply) {
+        try {
+            const paramsResult = paramsWithIdSchema.safeParse(request.params)
+            if (!paramsResult.success) {
+                return reply.status(400).send({
+                    message: "ID inválido",
+                    errors: formatError(paramsResult)
+                })
+            }
+
+            const bodyResult = updateUserPasswordSchema.safeParse(request.body)
+
+            if (!bodyResult.success) {
+                return reply.status(400).send({
+                    message: 'Dados inválidos',
+                    errors: formatError(bodyResult)
+                })
+            }
+
+            const userId = paramsResult.data.id
+            const data = bodyResult.data
+
+            const user = await usersService.updatePassword(userId, data)
+
+            return reply.send(user)
+
+
         } catch (error) {
             if (error instanceof Error) {
                 return reply.status(400).send({ message: error.message });
@@ -94,13 +170,15 @@ export const usersController = {
 
     async delete(request: FastifyRequest<{ Params: ParamsWithId }>, reply: FastifyReply) {
         try {
-            const { id } = request.params;
-            const userId = parseInt(id);
+            const result = paramsWithIdSchema.safeParse(request.params)
 
-            if (isNaN(userId)) {
-                return reply.status(400).send({ message: "ID Inválido" });
+            if (!result.success) {
+                return reply.status(400).send({
+                    message: "ID inválido",
+                    errors: formatError(result)
+                })
             }
-
+            const userId = result.data.id
             await usersService.delete(userId);
             return reply.status(204).send();
         } catch (error) {
