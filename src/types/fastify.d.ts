9917af15827cf0029c83b47024c5c98d760bfc1b
Daniel <daniellfmanoel@gmail.com>
2025-05-23 09:07:13 -0300

    ✨feat: implement authentication system with JWT, including login and logout functionality

--- src/types/fastify.d.ts
+++ src/types/fastify.d.ts
@@ -2,6 +2,9 @@ import 'fastify';
 
 declare module 'fastify' {
   interface FastifyRequest {
-    // Você pode adicionar propriedades personalizadas aqui se necessário
+    user?: {
+      id: string;
+
+    };
   }
 }
