965036ad65863b462f06594c12d2d4b08b1850dd
Daniel <daniellfmanoel@gmail.com>
2025-05-30 08:16:27 -0300

    🔧refactor: port changes

--- src/server.ts
+++ src/server.ts
@@ -19,9 +19,11 @@ app.register(productRoutes)
 
 
 
-const start = async() => {
+const start = async () => {
     try {
-        await app.listen({ port: 3000 })
+        const port = process.env.PORT ? parseInt(process.env.PORT) : 3000
+
+        await app.listen({ port, host: '0.0.0.0' })
         app.log.info(`Server listening on ${app.server.address()}`)
     } catch (err) {
         app.log.error(err)
